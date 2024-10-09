a4a71b48da4fe946b1cde248b9ae580bf8edd6fd
Yamnyr <quentinwaroquet@gmail.com>
2024-10-09 18:32:56 +0200

    securisation des touches

--- game.py
+++ game.py
@@ -11,7 +11,12 @@ class Casino:
         self.mise_max = 0
 
     def afficher_regles(self):
-        reponse = input("\033[36mSouhaitez-vous afficher les règles du jeu (O/N) ?\033[0m \n").strip().lower()
+        while True:
+            reponse = input("\033[36mSouhaitez-vous afficher les règles du jeu (O/N) ?\033[0m \n").strip().lower()
+            if reponse in ['o', 'n']:
+                break
+            print("\033[31mRéponse invalide. Veuillez entrer 'O' pour Oui ou 'N' pour Non.\033[0m")
+
         if reponse == 'o':
             print("""
             \033[34m* RÈGLES DU JEU *\033[0m
@@ -27,14 +32,13 @@ class Casino:
     def demande_saisie(self):
         try:
             self.nb_user = int(input("Entrez un nombre (en 10 secondes max) : \n"))
+            if self.nb_user < 1 or self.nb_user > self.user.level * 10:
+                raise ValueError
         except ValueError:
-            print("\033[31mEntrée invalide. Veuillez entrer un nombre.\033[0m")
+            print(f"\033[31mEntrée invalide. Veuillez entrer un nombre entre 1 et {self.user.level * 10}.\033[0m")
             self.nb_user = None
 
-    def jouer_tour(self):
-        nb_python = randrange(1, self.user.level * 10 + 1)
-        nb_essais = 3 + (2 * self.user.level - 2)
-
+    def demande_mise(self):
         while True:
             try:
                 mise = int(
@@ -44,14 +48,20 @@ class Casino:
                     print(
                         f"\033[31mErreur, votre mise est plus élevée que votre solde. Entrer une mise inférieure ou égale à {self.user.solde} € :\033[0m")
                 elif mise <= 0:
-                    print("\033[31mLe montant saisi n'est pas valide. Entrer SVP un montant entre 1 et 10 € :\033[0m")
+                    print("\033[31mLe montant saisi n'est pas valide. Entrer SVP un montant positif :\033[0m")
                 else:
-                    print(f"\033[36mVous avez misé {mise}.\033[0m")
-                    if mise > self.mise_max:
-                        self.mise_max = mise
-                    break
+                    return mise
             except ValueError:
-                print("\033[31mLe montant saisi n'est pas valide. Entrer SVP un montant entre 1 et 10 € :\033[0m")
+                print("\033[31mLe montant saisi n'est pas valide. Veuillez entrer un nombre entier positif.\033[0m")
+
+    def jouer_tour(self):
+        nb_python = randrange(1, self.user.level * 10 + 1)
+        nb_essais = 3 + (2 * self.user.level - 2)
+
+        mise = self.demande_mise()
+        print(f"\033[36mVous avez misé {mise}.\033[0m")
+        if mise > self.mise_max:
+            self.mise_max = mise
 
         print(f'\t- \033[36mAlors mon nombre est entre 1 et {self.user.level * 10}. Devinez lequel ?\033[0m')
         print(f'\t- \033[36mVous avez {nb_essais} essais.\033[0m')
@@ -71,9 +81,8 @@ class Casino:
 
             while thread_saisie.is_alive():
                 if time.time() - debut_essai > self.TEMPS_MAX:
-                    nb_essais -= 1
                     print(
-                        f"\033[31mVous avez dépassé le délai de {self.TEMPS_MAX} secondes ! Vous perdez l'essai courant et il vous reste {nb_essais} essai(s) !\033[0m")
+                        f"\033[31mVous avez dépassé le délai de {self.TEMPS_MAX} secondes ! Vous perdez cet essai !\033[0m")
                     thread_saisie.join()
                     break
 
@@ -103,6 +112,13 @@ class Casino:
         self.user.update_stats(victoire=False, gain=0, perte=mise, mise_max=self.mise_max)
         return False
 
+    def demande_oui_non(self, question):
+        while True:
+            reponse = input(question).strip().lower()
+            if reponse in ['o', 'n']:
+                return reponse == 'o'
+            print("\033[31mRéponse invalide. Veuillez entrer 'O' pour Oui ou 'N' pour Non.\033[0m")
+
     def start_game(self):
         self.afficher_regles()
         self.user.initialize()
@@ -113,13 +129,10 @@ class Casino:
 
             self.jouer_tour()
 
-            afficher_stats = input(
-                "\033[36mSouhaitez-vous consulter vos statistiques (O/N) ?\033[0m \n").strip().lower()
-            if afficher_stats == 'o':
+            if self.demande_oui_non("\033[36mSouhaitez-vous consulter vos statistiques (O/N) ?\033[0m \n"):
                 self.user.display_stats()
 
-            continuer = input("\033[36mSouhaitez-vous continuer la partie (O/N) ?\033[0m \n").strip().lower()
-            if continuer != 'o':
+            if not self.demande_oui_non("\033[36mSouhaitez-vous continuer la partie (O/N) ?\033[0m \n"):
                 print(f"\033[32mAu revoir ! Vous finissez la partie avec {self.user.solde} €.\033[0m")
                 break
             else:
@@ -131,9 +144,7 @@ class Casino:
                         f"\033[36mSuper ! Vous passez au Level 3.\nRappelez-vous, le principe est le même sauf que mon nombre est entre 1 et 30 et vous avez le droit à 7 essais !\033[0m")
                 elif self.user.level == 4:
                     print(f"\033[32mFélicitations {self.user.name}, vous avez atteint le niveau maximum !\033[0m")
-                    continuer = input(
-                        "\033[36mSouhaitez-vous recommencer depuis le Level 1 (O/N) ?\033[0m \n").strip().lower()
-                    if continuer == 'o':
+                    if self.demande_oui_non("\033[36mSouhaitez-vous recommencer depuis le Level 1 (O/N) ?\033[0m \n"):
                         self.user.level = 1
                         self.user.dernier_level = 1
                         self.user.update_score(self.user.level, self.user.dernier_level, self.user.solde)
